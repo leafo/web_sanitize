429f6e6c3099047c3f01f5a69e9ce92b70ec0378
leaf corcoran <leafot@gmail.com>
2016-04-29 00:05:14 -0700

    create sanitizer and extractor constructors

--- web_sanitize/html.lua
+++ web_sanitize/html.lua
@@ -3,121 +3,8 @@ do
   local _obj_0 = table
   insert, concat = _obj_0.insert, _obj_0.concat
 end
-local allowed_tags, add_attributes, self_closing
-do
-  local _obj_0 = require("web_sanitize.whitelist")
-  allowed_tags, add_attributes, self_closing = _obj_0.tags, _obj_0.add_attributes, _obj_0.self_closing
-end
 local unpack = unpack or table.unpack
 local lpeg = require("lpeg")
-local escape_text
-local tag_stack = { }
-local check_tag
-check_tag = function(str, pos, tag)
-  local lower_tag = tag:lower()
-  local allowed = allowed_tags[lower_tag]
-  if not (allowed) then
-    return false
-  end
-  insert(tag_stack, lower_tag)
-  return true, tag
-end
-local check_close_tag
-check_close_tag = function(str, pos, punct, tag, rest)
-  local lower_tag = tag:lower()
-  local top = #tag_stack
-  pos = top
-  while pos >= 1 do
-    if tag_stack[pos] == lower_tag then
-      break
-    end
-    pos = pos - 1
-  end
-  if pos == 0 then
-    return false
-  end
-  local buffer = { }
-  local k = 1
-  for i = top, pos + 1, -1 do
-    local _continue_0 = false
-    repeat
-      local next_tag = tag_stack[i]
-      tag_stack[i] = nil
-      if self_closing[next_tag] then
-        _continue_0 = true
-        break
-      end
-      buffer[k] = "</"
-      buffer[k + 1] = next_tag
-      buffer[k + 2] = ">"
-      k = k + 3
-      _continue_0 = true
-    until true
-    if not _continue_0 then
-      break
-    end
-  end
-  tag_stack[pos] = nil
-  buffer[k] = punct
-  buffer[k + 1] = tag
-  buffer[k + 2] = rest
-  return true, unpack(buffer)
-end
-local pop_tag
-pop_tag = function(str, pos, ...)
-  tag_stack[#tag_stack] = nil
-  return true, ...
-end
-local fail_tag
-fail_tag = function()
-  tag_stack[#tag_stack] = nil
-  return false
-end
-local check_attribute
-check_attribute = function(str, pos_end, pos_start, name, value)
-  local tag = tag_stack[#tag_stack]
-  local allowed_attributes = allowed_tags[tag]
-  if type(allowed_attributes) ~= "table" then
-    return true
-  end
-  local attr = allowed_attributes[name:lower()]
-  local new_val
-  if type(attr) == "function" then
-    new_val = attr(value, name, tag)
-    if not (new_val) then
-      return true
-    end
-  else
-    if not (attr) then
-      return true
-    end
-  end
-  if type(new_val) == "string" then
-    return true, " " .. tostring(name) .. "=\"" .. tostring(assert(escape_text:match(new_val))) .. "\""
-  else
-    return true, str:sub(pos_start, pos_end - 1)
-  end
-end
-local inject_attributes
-inject_attributes = function()
-  local top_tag = tag_stack[#tag_stack]
-  local inject = add_attributes[top_tag]
-  if inject then
-    local buff = { }
-    local i = 1
-    for k, v in pairs(inject) do
-      buff[i] = " "
-      buff[i + 1] = k
-      buff[i + 2] = '="'
-      buff[i + 3] = v
-      buff[i + 4] = '"'
-      i = i + 5
-    end
-    return true, unpack(buff)
-  else
-    return true
-  end
-end
 local R, S, V, P
 R, S, V, P = lpeg.R, lpeg.S, lpeg.V, lpeg.P
 local C, Cs, Ct, Cmt, Cg, Cb, Cc, Cp
@@ -130,7 +17,6 @@ local escaped_char = S("<>'&\"") / {
   ["/"] = "&#x2F;",
   ['"'] = "&quot;"
 }
-escape_text = Cs((escaped_char + 1) ^ 0 * -1)
 local alphanum = R("az", "AZ", "09")
 local num = R("09")
 local hex = R("09", "af", "AF")
@@ -140,48 +26,166 @@ local text = C((1 - escaped_char) ^ 1)
 local word = (alphanum + S("._-")) ^ 1
 local value = C(word) + P('"') * C((1 - P('"')) ^ 0) * P('"') + P("'") * C((1 - P("'")) ^ 0) * P("'")
 local attribute = C(word) * (white * P("=") * white * value) ^ -1
-local open_tag = C(P("<") * white) * Cmt(word, check_tag) * (Cmt(Cp() * white * attribute, check_attribute) ^ 0 * white * Cmt("", inject_attributes) * Cmt("/" * white, pop_tag) ^ -1 * C(">") + Cmt("", fail_tag))
-local close_tag = Cmt(C(P("<") * white * P("/") * white) * C(word) * C(white * P(">")), check_close_tag)
-local value_ignored = word + P('"') * (1 - P('"')) ^ 0 * P('"') + P("'") * (1 - P("'")) ^ 0 * P("'")
-local attribute_ignored = word * (white * P("=") * white * value_ignored) ^ -1
-local open_tag_ignored = P("<") * white * word * (white * attribute_ignored) ^ 0 * white * (P("/") * white) ^ -1 * P(">") / " "
-local close_tag_ignored = P("<") * white * P("/") * white * word * white * P(">") / " "
-local html = Ct((open_tag + close_tag + valid_char + escaped_char + text) ^ 0 * -1)
-local html_text = Ct((open_tag_ignored + close_tag_ignored + valid_char + escaped_char + text) ^ 0 * -1)
-local sanitize_html
-sanitize_html = function(str)
-  tag_stack = { }
-  local buffer = assert(html:match(str), "failed to parse html")
-  local k = #buffer + 1
-  for i = #tag_stack, 1, -1 do
-    local _continue_0 = false
-    repeat
-      local tag = tag_stack[i]
-      if self_closing[tag] then
-        _continue_0 = true
+local escape_text = Cs((escaped_char + 1) ^ 0 * -1)
+local Sanitizer
+Sanitizer = function(opts)
+  local allowed_tags, add_attributes, self_closing
+  do
+    local _obj_0 = opts and opts.whitelist or require("web_sanitize.whitelist")
+    allowed_tags, add_attributes, self_closing = _obj_0.tags, _obj_0.add_attributes, _obj_0.self_closing
+  end
+  local tag_stack = { }
+  local check_tag
+  check_tag = function(str, pos, tag)
+    local lower_tag = tag:lower()
+    local allowed = allowed_tags[lower_tag]
+    if not (allowed) then
+      return false
+    end
+    insert(tag_stack, lower_tag)
+    return true, tag
+  end
+  local check_close_tag
+  check_close_tag = function(str, pos, punct, tag, rest)
+    local lower_tag = tag:lower()
+    local top = #tag_stack
+    pos = top
+    while pos >= 1 do
+      if tag_stack[pos] == lower_tag then
         break
       end
-      buffer[k] = "</"
-      buffer[k + 1] = tag
-      buffer[k + 2] = ">"
-      k = k + 3
-      _continue_0 = true
-    until true
-    if not _continue_0 then
-      break
+      pos = pos - 1
+    end
+    if pos == 0 then
+      return false
+    end
+    local buffer = { }
+    local k = 1
+    for i = top, pos + 1, -1 do
+      local _continue_0 = false
+      repeat
+        local next_tag = tag_stack[i]
+        tag_stack[i] = nil
+        if self_closing[next_tag] then
+          _continue_0 = true
+          break
+        end
+        buffer[k] = "</"
+        buffer[k + 1] = next_tag
+        buffer[k + 2] = ">"
+        k = k + 3
+        _continue_0 = true
+      until true
+      if not _continue_0 then
+        break
+      end
+    end
+    tag_stack[pos] = nil
+    buffer[k] = punct
+    buffer[k + 1] = tag
+    buffer[k + 2] = rest
+    return true, unpack(buffer)
+  end
+  local pop_tag
+  pop_tag = function(str, pos, ...)
+    tag_stack[#tag_stack] = nil
+    return true, ...
+  end
+  local fail_tag
+  fail_tag = function()
+    tag_stack[#tag_stack] = nil
+    return false
+  end
+  local check_attribute
+  check_attribute = function(str, pos_end, pos_start, name, value)
+    local tag = tag_stack[#tag_stack]
+    local allowed_attributes = allowed_tags[tag]
+    if type(allowed_attributes) ~= "table" then
+      return true
+    end
+    local attr = allowed_attributes[name:lower()]
+    local new_val
+    if type(attr) == "function" then
+      new_val = attr(value, name, tag)
+      if not (new_val) then
+        return true
+      end
+    else
+      if not (attr) then
+        return true
+      end
+    end
+    if type(new_val) == "string" then
+      return true, " " .. tostring(name) .. "=\"" .. tostring(assert(escape_text:match(new_val))) .. "\""
+    else
+      return true, str:sub(pos_start, pos_end - 1)
     end
   end
-  return concat(buffer)
+  local inject_attributes
+  inject_attributes = function()
+    local top_tag = tag_stack[#tag_stack]
+    local inject = add_attributes[top_tag]
+    if inject then
+      local buff = { }
+      local i = 1
+      for k, v in pairs(inject) do
+        buff[i] = " "
+        buff[i + 1] = k
+        buff[i + 2] = '="'
+        buff[i + 3] = v
+        buff[i + 4] = '"'
+        i = i + 5
+      end
+      return true, unpack(buff)
+    else
+      return true
+    end
+  end
+  local tag_attributes = Cmt(Cp() * white * attribute, check_attribute) ^ 0
+  local open_tag = C(P("<") * white) * Cmt(word, check_tag) * (tag_attributes * white * Cmt("", inject_attributes) * Cmt("/" * white, pop_tag) ^ -1 * C(">") + Cmt("", fail_tag))
+  local close_tag = Cmt(C(P("<") * white * P("/") * white) * C(word) * C(white * P(">")), check_close_tag)
+  local html = Ct((open_tag + close_tag + valid_char + escaped_char + text) ^ 0 * -1)
+  return function(str)
+    tag_stack = { }
+    local buffer = assert(html:match(str), "failed to parse html")
+    local k = #buffer + 1
+    for i = #tag_stack, 1, -1 do
+      local _continue_0 = false
+      repeat
+        local tag = tag_stack[i]
+        if self_closing[tag] then
+          _continue_0 = true
+          break
+        end
+        buffer[k] = "</"
+        buffer[k + 1] = tag
+        buffer[k + 2] = ">"
+        k = k + 3
+        _continue_0 = true
+      until true
+      if not _continue_0 then
+        break
+      end
+    end
+    return concat(buffer)
+  end
 end
-local extract_text
-extract_text = function(str)
-  local buffer = assert(html_text:match(str), "failed to parse html")
-  local out = concat(buffer)
-  out = out:gsub("%s+", " ")
-  return (out:match("^%s*(.-)%s*$"))
+local Extractor
+Extractor = function(opts)
+  local value_ignored = word + P('"') * (1 - P('"')) ^ 0 * P('"') + P("'") * (1 - P("'")) ^ 0 * P("'")
+  local attribute_ignored = word * (white * P("=") * white * value_ignored) ^ -1
+  local open_tag_ignored = P("<") * white * word * (white * attribute_ignored) ^ 0 * white * (P("/") * white) ^ -1 * P(">") / " "
+  local close_tag_ignored = P("<") * white * P("/") * white * word * white * P(">") / " "
+  local html_text = Ct((open_tag_ignored + close_tag_ignored + valid_char + escaped_char + text) ^ 0 * -1)
+  return function(str)
+    local buffer = assert(html_text:match(str), "failed to parse html")
+    local out = concat(buffer)
+    out = out:gsub("%s+", " ")
+    return (out:match("^%s*(.-)%s*$"))
+  end
 end
 return {
-  sanitize_html = sanitize_html,
-  extract_text = extract_text,
+  Sanitizer = Sanitizer,
+  Extractor = Extractor,
   escape_text = escape_text
 }
--- web_sanitize/html.moon
+++ web_sanitize/html.moon
@@ -1,98 +1,7 @@
 import insert, concat from table
-
-{ tags: allowed_tags, :add_attributes, :self_closing } = require "web_sanitize.whitelist"
-
 unpack = unpack or table.unpack
 
 lpeg = require "lpeg"
-
-local escape_text
-
-tag_stack = {}
-
-check_tag = (str, pos, tag) ->
-  lower_tag = tag\lower!
-  allowed = allowed_tags[lower_tag]
-  return false unless allowed
-  insert tag_stack, lower_tag
-  true, tag
-
-check_close_tag = (str, pos, punct, tag, rest) ->
-  lower_tag = tag\lower!
-  top = #tag_stack
-  pos = top -- holds position in stack where what we are closing is
-
-  while pos >= 1
-    break if tag_stack[pos] == lower_tag
-    pos -= 1
-
-  if pos == 0
-    return false
-
-  buffer = {}
-
-  k = 1
-  for i=top, pos + 1, -1
-    next_tag = tag_stack[i]
-    tag_stack[i] = nil
-    continue if self_closing[next_tag]
-    buffer[k] = "</"
-    buffer[k + 1] = next_tag
-    buffer[k + 2] = ">"
-    k += 3
-
-  tag_stack[pos] = nil
-
-  buffer[k] = punct
-  buffer[k + 1] = tag
-  buffer[k + 2] = rest
-
-  true, unpack buffer
-
-pop_tag = (str, pos, ...) ->
-  tag_stack[#tag_stack] = nil
-  true, ...
-
-fail_tag = ->
-  tag_stack[#tag_stack] = nil
-  false
-
-check_attribute = (str, pos_end, pos_start, name, value) ->
-  tag = tag_stack[#tag_stack]
-  allowed_attributes = allowed_tags[tag]
-
-  if type(allowed_attributes) != "table"
-    return true
-
-  attr = allowed_attributes[name\lower!]
-  local new_val
-  if type(attr) == "function"
-    new_val = attr value, name, tag
-    return true unless new_val
-  else
-    return true unless attr
-
-  if type(new_val) == "string"
-    true, " #{name}=\"#{assert escape_text\match new_val}\""
-  else
-    true, str\sub pos_start, pos_end - 1
-
-inject_attributes = ->
-  top_tag = tag_stack[#tag_stack]
-  inject = add_attributes[top_tag]
-  if inject
-    buff = {}
-    i = 1
-    for k,v in pairs inject
-      buff[i] = " "
-      buff[i + 1] = k
-      buff[i + 2] = '="'
-      buff[i + 3] = v
-      buff[i + 4] = '"'
-      i += 5
-    true, unpack buff
-  else
-    true
 
 import R, S, V, P from lpeg
 import C, Cs, Ct, Cmt, Cg, Cb, Cc, Cp from lpeg
@@ -106,7 +15,6 @@ escaped_char = S"<>'&\"" / {
   '"': "&quot;"
 }
 
-escape_text = Cs (escaped_char + 1)^0 * -1
 
 alphanum = R "az", "AZ", "09"
 num = R "09"
@@ -119,42 +27,143 @@ text = C (1 - escaped_char)^1
 word = (alphanum + S"._-")^1
 
 value = C(word) + P'"' * C((1 - P'"')^0) * P'"' + P"'" * C((1 - P"'")^0) * P"'"
-
 attribute = C(word) * (white * P"=" * white * value)^-1
 
-open_tag = C(P"<" * white) * Cmt(word, check_tag) * (Cmt(Cp! * white * attribute, check_attribute)^0 * white * Cmt("", inject_attributes) * Cmt("/" * white, pop_tag)^-1 * C">" + Cmt("", fail_tag))
-close_tag = Cmt(C(P"<" * white * P"/" * white) * C(word) * C(white * P">"), check_close_tag)
+escape_text = Cs (escaped_char + 1)^0 * -1
 
--- parses through tags without capturing anything
-value_ignored = word + P'"' * (1 - P'"')^0 * P'"' + P"'" * (1 - P"'")^0 * P"'"
-attribute_ignored = word * (white * P"=" * white * value_ignored)^-1
-open_tag_ignored = P"<" * white * word * (white * attribute_ignored)^0 * white * (P"/" * white)^-1 * P">" / " "
-close_tag_ignored = P"<" * white * P"/" * white * word * white * P">" / " "
+Sanitizer = (opts) ->
+  {
+    tags: allowed_tags, :add_attributes, :self_closing
+  } = opts and opts.whitelist or require "web_sanitize.whitelist"
 
-html = Ct (open_tag + close_tag + valid_char + escaped_char + text)^0 * -1
-
-html_text = Ct (open_tag_ignored + close_tag_ignored + valid_char + escaped_char + text)^0 * -1
-
-sanitize_html = (str) ->
   tag_stack = {}
-  buffer = assert html\match(str), "failed to parse html"
-  k = #buffer + 1
-  for i=#tag_stack,1,-1
-    tag = tag_stack[i]
-    continue if self_closing[tag]
-    buffer[k] = "</"
-    buffer[k + 1] = tag
-    buffer[k + 2] = ">"
-    k += 3
 
-  concat buffer
+  check_tag = (str, pos, tag) ->
+    lower_tag = tag\lower!
+    allowed = allowed_tags[lower_tag]
+    return false unless allowed
+    insert tag_stack, lower_tag
+    true, tag
+
+  check_close_tag = (str, pos, punct, tag, rest) ->
+    lower_tag = tag\lower!
+    top = #tag_stack
+    pos = top -- holds position in stack where what we are closing is
+
+    while pos >= 1
+      break if tag_stack[pos] == lower_tag
+      pos -= 1
+
+    if pos == 0
+      return false
+
+    buffer = {}
+
+    k = 1
+    for i=top, pos + 1, -1
+      next_tag = tag_stack[i]
+      tag_stack[i] = nil
+      continue if self_closing[next_tag]
+      buffer[k] = "</"
+      buffer[k + 1] = next_tag
+      buffer[k + 2] = ">"
+      k += 3
+
+    tag_stack[pos] = nil
+
+    buffer[k] = punct
+    buffer[k + 1] = tag
+    buffer[k + 2] = rest
+
+    true, unpack buffer
+
+  pop_tag = (str, pos, ...) ->
+    tag_stack[#tag_stack] = nil
+    true, ...
+
+  fail_tag = ->
+    tag_stack[#tag_stack] = nil
+    false
+
+  check_attribute = (str, pos_end, pos_start, name, value) ->
+    tag = tag_stack[#tag_stack]
+    allowed_attributes = allowed_tags[tag]
+
+    if type(allowed_attributes) != "table"
+      return true
+
+    attr = allowed_attributes[name\lower!]
+    local new_val
+    if type(attr) == "function"
+      new_val = attr value, name, tag
+      return true unless new_val
+    else
+      return true unless attr
+
+    if type(new_val) == "string"
+      true, " #{name}=\"#{assert escape_text\match new_val}\""
+    else
+      true, str\sub pos_start, pos_end - 1
+
+  inject_attributes = ->
+    top_tag = tag_stack[#tag_stack]
+    inject = add_attributes[top_tag]
+    if inject
+      buff = {}
+      i = 1
+      for k,v in pairs inject
+        buff[i] = " "
+        buff[i + 1] = k
+        buff[i + 2] = '="'
+        buff[i + 3] = v
+        buff[i + 4] = '"'
+        i += 5
+      true, unpack buff
+    else
+      true
+
+  tag_attributes = Cmt(Cp! * white * attribute, check_attribute)^0
+
+  open_tag = C(P"<" * white) *
+    Cmt(word, check_tag) *
+    (tag_attributes * white * Cmt("", inject_attributes) * Cmt("/" * white, pop_tag)^-1 * C">" + Cmt("", fail_tag))
+
+  close_tag = Cmt(C(P"<" * white * P"/" * white) * C(word) * C(white * P">"), check_close_tag)
+
+  html = Ct (open_tag + close_tag + valid_char + escaped_char + text)^0 * -1
+
+  (str) ->
+    tag_stack = {}
+    buffer = assert html\match(str), "failed to parse html"
+    k = #buffer + 1
+    for i=#tag_stack,1,-1
+      tag = tag_stack[i]
+      continue if self_closing[tag]
+      buffer[k] = "</"
+      buffer[k + 1] = tag
+      buffer[k + 2] = ">"
+      k += 3
+
+    concat buffer
 
 -- parse the html, extract text between non tag items
-extract_text = (str) ->
-  buffer = assert html_text\match(str), "failed to parse html"
-  out = concat buffer
-  out = out\gsub "%s+", " "
-  (out\match "^%s*(.-)%s*$")
+Extractor = (opts) ->
+  -- parses through tags without capturing anything
+  value_ignored = word + P'"' * (1 - P'"')^0 * P'"' + P"'" * (1 - P"'")^0 * P"'"
 
-{ :sanitize_html, :extract_text, :escape_text }
+  attribute_ignored = word * (white * P"=" * white * value_ignored)^-1
+
+  open_tag_ignored = P"<" * white * word * (white * attribute_ignored)^0 * white * (P"/" * white)^-1 * P">" / " "
+  close_tag_ignored = P"<" * white * P"/" * white * word * white * P">" / " "
+
+  html_text = Ct (open_tag_ignored + close_tag_ignored + valid_char + escaped_char + text)^0 * -1
+
+  (str) ->
+    buffer = assert html_text\match(str), "failed to parse html"
+    out = concat buffer
+    out = out\gsub "%s+", " "
+    (out\match "^%s*(.-)%s*$")
+
+
+{ :Sanitizer, :Extractor, :escape_text }
 
